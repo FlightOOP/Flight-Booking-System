df80fc3c476b726b1b05ceaa6cfee4b49c3047c3
HA0071999 <24-0613@student.gutech.edu.om>
2026-05-01 18:43:08 +0400

    Update classes.cpp

--- classes.cpp
+++ classes.cpp
@@ -2,23 +2,53 @@
 using namespace std;
 
 class User{
-
+    public:
+    string user_name;
+    User(){}
+    protected:
+    string password;
+    public: //BOTH ADMIN AND GYEST USE THIS
+    void login(string usrnm){
+        if (usrnm==user_name){
+            string tempp;
+            cout<<"Enter password: "<<endl;
+            cin>>tempp;
+            if (tempp==password){
+                cout<<"Login succesful! Welcome "<<user_name;
+            }
+            else {cout<<"Incorrect password. ";}
+        }
+            
+    }
 };
 class Guest : public User{ //inherits from User
-  
+    public:
+    Guest(string us, const string psswd) {
+        user_name=us;
+        password=psswd;
+    }
+    
 };
 
 class Admin: public User { //inherits from User
+    public:
+    Admin(string us, const string psswd) {
+        user_name=us;
+        password=psswd;
+    }
 };
 
 
 class Booking{
     
 };
+
 class Flight{
-    Gates* gate; //
     Booking bookings;
     //one flight has array of bookings
 };
 
-
+int main(){
+    Guest  u1("Name","abc123");
+    u1.login("Name");
+}
