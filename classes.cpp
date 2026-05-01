e72d377862888ce27290f1403833c3b1ff647b24
tyoom-aziz <24-0080@student.gutech.edu.om>
2026-05-01 20:06:51 +0400

    classes.cpp

--- classes.cpp
+++ classes.cpp
@@ -7,7 +7,7 @@ class User{
     User(){}
     protected:
     string password;
-    public: //BOTH ADMIN AND GYEST USE THIS
+    public: //BOTH ADMIN AND GUEST USE THIS
     void login(string usrnm){
         if (usrnm==user_name){
             string tempp;
@@ -40,8 +40,39 @@ class Admin: public User { //inherits from User
 
 
 class Booking{
-    
+protected:
+    string destinationFrom, destinationTo, date, time, gateNo, seatClass, flight;
+    int seatNo;
+public:
+    //pssngrInfo is a friend of user (to get his info)
+    void passengerInfo(){}
+    void cancelBooking(){}
+    void bookingInfo(){}
 };
+void Booking::passengerInfo(){
+    cout << "Full Name: " << endl;
+    cout << "Phone Number: " << endl;
+}
+void Booking::cancelBooking(){
+    int n;
+    cout << "Enter 1 to cancle your booking. \nEnter 0 to continue without canceling: ";
+    cin >> n;
+    if (n==1){cout << "Booking cancelled.\n";}
+    else {cout << "Booking confirmed\n";}
+}
+void Booking::bookingInfo(){
+    cout << "Your Booking has been Confirmed. Please Don't loose Your ticket.\n";
+    cout << "Passenger Name: " << endl;
+    cout << "Date: " << date << endl;
+    cout << "Flight: " << flight << endl;
+    cout << endl;
+    cout << "Boarding Time: " << time << endl;
+    cout << endl;
+    cout << "Destination from " << destinationFrom << " to " << destinationTo << endl;
+    cout << "class: " << seatClass << endl;
+    cout << "Seat Number: " << seatNo << endl;
+    cout << "Gate Number: " << gateNo << endl;
+}
 
 class Flight{
     Booking bookings;
