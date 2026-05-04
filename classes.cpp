3425150540b8ff58255e4b68e31d36751b7b2b1f
tyoom-aziz <24-0080@student.gutech.edu.om>
2026-05-04 21:01:20 +0400

    classes.cpp

--- classes.cpp
+++ classes.cpp
@@ -82,6 +82,9 @@ protected:
     string destinationFrom, destinationTo, date, time, gateNo, seatClass, flight;
     int seatNo;
 public:
+Booking(string destinationFrom, string destinationTo, string date, string time, string gateNo, string seatClass,
+    string flight) : destinationFrom(destinationFrom), destinationTo(destinationTo), date(date),
+    time(time), gateNo(gateNo), seatClass(seatClass), flight(flight){}
     //pssngrInfo is a friend of user (to get his info)
     void passengerInfo();
     void cancelBooking();
