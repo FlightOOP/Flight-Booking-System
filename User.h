26e3b9a5e77fb7ce0ffb997f6f872ed82889a66b
Anfal <24-0331@student.gutech.edu.om>
2026-05-13 12:22:44 +0400

    Update User.h
    Added Class User code in this file

--- User.h
+++ User.h
@@ -1,1 +1,53 @@
+#ifndef L1_USER_H
+#define L1_USER_H
+#include <iostream>
+#include <string>
+#include<vector>
+using namespace std;
 
+class User{
+protected:
+    string user_name;
+
+private:
+    string password;
+    long int phone_no;
+    string bank_no;
+
+public:
+    User(){}
+    void setPasswword(string p){
+        password=p;
+    }
+    string getPassword(){
+        return password;
+    }
+
+    void setPhone_no(long int phno){
+        if (to_string(phno).length()==8){
+            phone_no=phno;
+        }
+        else {cout<<"Phone number must be 8 digits long"<<endl;}
+    }
+    long int getPhone_no(){
+        return phone_no;
+    }
+
+    void setBank_no(string b){
+        bank_no=b;
+    }
+    string getBank_no(){
+        return bank_no;
+    }
+    void setUser_name(string us){
+        user_name=us;
+    }
+    string getUser_name(){
+        return user_name;
+    }
+    //BOTH ADMIN AND GUEST USE THIS
+    virtual void login(string usrnm)=0;
+};
+
+
+#endif //L1_USER_H
