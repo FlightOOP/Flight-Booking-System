aca7c1a63bd92831f72a94cb93ca7e8ea1ce4898
Anfal <24-0331@student.gutech.edu.om>
2026-05-13 08:51:49 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -27,5 +27,40 @@ int main(){
         g1.login("Guest 1");
     }
 
+    
+    cout<<"\n-----------------------------------------------------------"<<endl;
+    cout<<"Available Flights: "<<endl;
+    f1.display_flight();                                                        //My prebuilt flight (Placeholder)
+    //an array that displays our available flights
+
+    cout<<"------------------------------------------------------------"<<endl;
+    cout<<"\nPlease select your next action: (Enter the number of action desirable): "<<endl;
+    cout<<"1.Book a flight \n2.Cancel my booking"                              //What my user can do if they are a guest
+          " \n3.My Booking info  \n4.User Settings"<<endl;
+    cout<<"\n-------------------------------------------------------------"<<endl;
+
+    int choice;
+    cin>>choice;
+
+    switch (choice)
+    {
+        case 1: cout<<"Please enter the number of the flight you want to book"<<endl;
+            int flight_number;
+            cin>>flight_number;                                                 //Should sift through the array of flights
+            f1.bookFlight();
+            break;
+
+        case 2: //Function to cancel by booking here
+            break;
+
+        case 3: //Function to display my booking info here
+            break;
+
+        case 4: //Function for user info manipulation here
+            break;
+        default: cout<<"Session expired";
+            break;
+    }
+
 return 0;
 }
