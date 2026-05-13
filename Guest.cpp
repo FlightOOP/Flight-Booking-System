a429293cba73636869f359fcda2194fefb596ce7
Anfal <24-0331@student.gutech.edu.om>
2026-05-13 12:37:40 +0400

    Create Guest.cpp

---
+++ Guest.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include<vector>
+#include<cstdlib>
+#include<ctime>
+#include "Guest.h"
+#include "User.h"
+using namespace std;
+
+//Guest member functions here
