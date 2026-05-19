c3edcdc130bde39c33685154da593345f2233ab3
HA0071999 <24-0613@student.gutech.edu.om>
2026-05-19 19:13:39 +0400

    Update classes.cpp

--- classes.cpp
+++ classes.cpp
@@ -55,6 +55,7 @@ protected:
     string destinationFrom, destinationTo, date, time, gateNo, seatClass, flight;
     int seatNo;
     bool isCancelled;
+
 public:
 Booking(User* u , string destinationFrom, string destinationTo, string date, string time, string gateNo, string seatClass,
     string flight, int seatNo) : user(u), destinationFrom(destinationFrom), destinationTo(destinationTo), date(date),
