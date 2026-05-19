45fce6990d85609731712110dde3d0fb2d6c31fd
HA0071999 <24-0613@student.gutech.edu.om>
2026-05-19 18:51:53 +0400

    Update classes.cpp

--- classes.cpp
+++ classes.cpp
@@ -4,7 +4,49 @@
 #include<cstdlib>
 #include<ctime>
 using namespace std;
+class User{
+    protected:
+    string user_name;
+    
+    private:
+    string password;
+    long int phone_no;
+    string bank_no;
 
+    public: 
+    User(){} 
+    void setPasswword(string p){
+        password=p;
+    }
+    string getPassword(){
+        return password;
+    }
+
+    void setPhone_no(long int phno){
+        if (to_string(phno).length()==8){
+        phone_no=phno;
+        }
+        else {cout<<"Phone number must be 8 digits long"<<endl;}
+    }
+    long int getPhone_no(){
+        return phone_no;
+    }
+
+    void setBank_no(string b){
+        bank_no=b;
+    }
+    string getBank_no(){
+        return bank_no;
+    }
+    void setUser_name(string us){
+        user_name=us;
+    }
+    string getUser_name(){
+        return user_name;
+    }
+    //BOTH ADMIN AND GUEST USE THIS
+    virtual void login(string usrnm)=0;
+};
 class Flight  {
 
 private:
@@ -107,49 +149,7 @@ int Flight :: countBooking;
 
 
 
-class User{
-    protected:
-    string user_name;
-    
-    private:
-    string password;
-    long int phone_no;
-    string bank_no;
 
-    public: 
-    User(){} 
-    void setPasswword(string p){
-        password=p;
-    }
-    string getPassword(){
-        return password;
-    }
-
-    void setPhone_no(long int phno){
-        if (to_string(phno).length()==8){
-        phone_no=phno;
-        }
-        else {cout<<"Phone number must be 8 digits long"<<endl;}
-    }
-    long int getPhone_no(){
-        return phone_no;
-    }
-
-    void setBank_no(string b){
-        bank_no=b;
-    }
-    string getBank_no(){
-        return bank_no;
-    }
-    void setUser_name(string us){
-        user_name=us;
-    }
-    string getUser_name(){
-        return user_name;
-    }
-    //BOTH ADMIN AND GUEST USE THIS
-    virtual void login(string usrnm)=0;
-};
 class Guest : public User{ //inherits from User
     public:
     Guest(string us) {
