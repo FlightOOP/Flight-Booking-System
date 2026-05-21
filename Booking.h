b3e3559e0f3234c58234baec0ccfd979ca4a1f0a
tyoom-aziz <24-0080@student.gutech.edu.om>
2026-05-21 19:49:17 +0400

    Booking.h

--- Booking.h
+++ Booking.h
@@ -8,7 +8,7 @@ using namespace std;
 #define L1_BOOKING_H
 
 
-class Booking{
+class Booking:
 protected:
     User *user;//composition between user and booking
     string destinationFrom, destinationTo, date, time, gateNo, seatClass, flight;
@@ -16,14 +16,14 @@ protected:
     bool isCancelled;
 
 public:
-Booking(User* u , string destinationFrom, string destinationTo, string date, string time, string gateNo, string seatClass,
+    Booking(User* u , string destinationFrom, string destinationTo, string date, string time, string gateNo, string seatClass,
     string flight, int seatNo) : user(u), destinationFrom(destinationFrom), destinationTo(destinationTo), date(date),
     time(time), gateNo(gateNo), seatClass(seatClass), flight(flight), seatNo(seatNo), isCancelled(false){}
     //pssngrInfo is a friend of user (to get his info)
     void passengerInfo();
     void cancelBooking();
     void bookingInfo();
-};
+}:
 
 
 #endif //L1_BOOKING_H
