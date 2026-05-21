2233d45500337c61bfcc59c817e58dd6db7f481b
HA0071999 <24-0613@student.gutech.edu.om>
2026-05-21 18:26:12 +0400

    Update Admin.cpp

--- Admin.cpp
+++ Admin.cpp
@@ -7,6 +7,26 @@ using namespace std;
 #include "Admin.h"
 
 //Admin member functions here
- void login() override;
+void Admn::login() override {
+        string usrnm;
+        cout<<"Enter your username: ";
+        cin>>usrnm;
+        
+        if (usrnm==user_name){
+            string tempp;
+            cout<<"Enter password Admin: "<<endl;
+            cin>>tempp;
+            string p=getPassword();
+            if (tempp==p){ //user enetrs correct password
+                cout<<"Login succesful! Welcome "<<user_name;
+            }
+            else {cout<<"Incorrect password. ";}
+        }    
+    }
     //ONLY ADMIN CAN CREATE FLIGHTS!!!!!!!!
-     void createFlight(Flight &f, string departure_from, string arrival_dest, int flight_num, string destination );
+     void Admin::createFlight(Flight &f, string departure_from, string arrival_dest, int flight_num, string destination ){
+        f.setDeparture(departure_from);
+        f.setArrival(arrival_dest);
+        f.setFlightNum(flight_num);
+        f.setDestination(destination);
+    }
