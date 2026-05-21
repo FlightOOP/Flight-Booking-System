c8b4a9ddb6f5921e626c0ea925b57ed55added29
HA0071999 <24-0613@student.gutech.edu.om>
2026-05-21 18:27:16 +0400

    Update Guest.cpp

--- Guest.cpp
+++ Guest.cpp
@@ -7,4 +7,18 @@
 #include "User.h"
 using namespace std;
 
-//Guest member functions here
+void Guest::login() override {
+        string usrnm;
+        cout<<"Enter your username: ";
+        cin>>usrnm;
+        if (usrnm==user_name){
+            string tempp;
+            cout<<"Enter password Guest: "<<endl;
+            cin>>tempp;
+            string p=getPassword();
+            if (tempp==p){ //user enetrs correct password
+                cout<<"Login succesful! Welcome "<<user_name;
+            }
+            else {cout<<"Incorrect password. ";}
+        }    
+    }
