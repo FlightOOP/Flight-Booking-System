5f069a147ab8b0ccb084ac70ac9bf72359ab341c
HA0071999 <24-0613@student.gutech.edu.om>
2026-05-21 17:05:43 +0400

    Update Admin.h

--- Admin.h
+++ Admin.h
@@ -9,11 +9,15 @@ using namespace std;
 #include "User.h"
 
 class Admin: public User { //inherits from User
-public:
+    public:
     Admin(string us) {
         user_name=us;
     }
-    void login(string usrnm) override {
+    void login() override {
+        string usrnm;
+        cout<<"Enter your username: ";
+        cin>>usrnm;
+        
         if (usrnm==user_name){
             string tempp;
             cout<<"Enter password Admin: "<<endl;
@@ -23,9 +27,15 @@ public:
                 cout<<"Login succesful! Welcome "<<user_name;
             }
             else {cout<<"Incorrect password. ";}
-        }
+        }    
+    }
+    //ONLY ADMIN CAN CREATE FLIGHTS!!!!!!!!
+     void createFlight(Flight &f, string departure_from, string arrival_dest, int flight_num, string destination ){
+        f.setDeparture(departure_from);
+        f.setArrival(arrival_dest);
+        f.setFlightNum(flight_num);
+        f.setDestination(destination);
     }
 };
-
 
 #endif //L1_ADMIN_H
