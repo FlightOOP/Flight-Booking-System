aad79d3dc754829394271385f4a3bb898488bcf7
HA0071999 <24-0613@student.gutech.edu.om>
2026-05-21 18:31:45 +0400

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -4,4 +4,9 @@
 #include "User.h"
 using namespace std;
 
-//User member functions here
+void User::setPhone_no(long int phno){
+        if (to_string(phno).length()==8){
+        phone_no=phno;
+        }
+        else {cout<<"Phone number must be 8 digits long"<<endl;}
+    }
