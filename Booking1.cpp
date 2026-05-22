607007b6dce06f0f6f3ad9591ad7ca0f42bcb669
tyoom-aziz <24-0080@student.gutech.edu.om>
2026-05-22 19:58:38 +0400

    Booking1.cpp

---
+++ Booking1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include<vector>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+#include "Booking.h"
+
+void Booking::passengerInfo(){
+    cout << "Full Name: " << user->getUser_name() << endl;
+    cout << "Phone Number: " << user->getPhone_no() << endl;
+}
+void Booking::cancelBooking(){
+    isCancelled = true;
+    cout << "Booking cancelled.\n";
+}
+void Booking::bookingInfo(){
+    cout << "\n----- Booking Info -----\n";
+
+    if (isCancelled){
+        cout << "Status: Cancelled\n";
+    } else {
+        cout << "Status: Confirmed\n";
+        cout << "Passenger Name: " << user->getUser_name() << endl;
+        cout << "Date: " << date << endl;
+        cout << "Flight: " << flight << endl;
+        cout << endl;
+        cout << "Boarding Time: " << time << endl;
+        cout << endl;
+        cout << "Destination from " << destinationFrom << " to " << destinationTo << endl;
+        cout << "class: " << seatClass << endl;
+        cout << "Seat Number: " << seatNo << endl;
+        cout << "Gate Number: " << gateNo << endl;
+    }
+}
