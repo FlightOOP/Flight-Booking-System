51af4d20ee2e8d89c5ba36ff675ce9dad342440e
HA0071999 <24-0613@student.gutech.edu.om>
2026-05-23 14:22:36 +0400

    Update Admin.h

--- Admin.h
+++ Admin.h
@@ -17,6 +17,7 @@ class Admin: public User { //inherits from User
     
     //ONLY ADMIN CAN CREATE FLIGHTS!!!!!!!!
      void createFlight(Flight &f, string departure_from, string arrival_dest, int flight_num, string destination ); 
+    ~Admin();
 };
 
 #endif //L1_ADMIN_H
